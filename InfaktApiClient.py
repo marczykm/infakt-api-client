e46807af25d00cb0c4b3c2fce254cc0f11ebb60e
Marcin Marczyk <m.l.marczyk@gmail.com>
2021-11-03 12:52:11 +0100

    new api calls

--- InfaktApiClient.py
+++ InfaktApiClient.py
@@ -38,9 +38,57 @@ class InfaktApiClient:
         except Exception as yaml_error:
             raise Exception('Configuration problem: {error}'.format(error=yaml_error))
 
-    def findInvoice(self, invoiceNum):
+    def _prepareHeaders(self):
         headers = {'X-inFakt-ApiKey': self.config['infakt']['x-api-key']}
-        response = requests.get(self.INFAKT_API_URL + "invoices/" + str(invoiceNum) + ".json", headers=headers)
+        return headers
+
+    def findInvoice(self, invoiceNum):
+        response = requests.get(self.INFAKT_API_URL + "invoices/" + str(invoiceNum) + ".json", headers=self._prepareHeaders())
         invoice = Invoice(response.json())
         log.info(invoice)
         return invoice
+
+    def generatePdf(self, invoiceNum):
+        response = requests.get(self.INFAKT_API_URL + "/invoices/" + str(invoiceNum) + "/pdf.json?document_type=original&locale=pe", headers=self._prepareHeaders())
+        fileName = str(invoiceNum)+".pdf"
+        with open(fileName, 'wb') as fd:
+            for chunk in response.iter_content(chunk_size=128):
+                fd.write(chunk)
+            fd.close()
+        log.info("Saved invoice as {}".format(fileName))
+    
+    def sendViaEmail(self, invoiceNum, email, sendCopy=True):
+        data = { "print_type":"original", "locale":"pe", "recipient":email, "send_copy": sendCopy }
+        response = requests.post(self.INFAKT_API_URL + "invoices/" + str(invoiceNum) + "/deliver_via_email.json", headers=self._prepareHeaders(), data=data)
+        
+        log.info(response.status_code)
+    
+    def createInvoice(self, clientId, saleDate, invoiceDate, paymentDate, grossPrice):
+        payload = {
+            "invoice": {
+                "client_id": str(clientId),
+                "payment_method": "transfer",
+                "sale_date": saleDate,
+                "invoice_date": invoiceDate,
+                "payment_date": paymentDate,
+                "services": [
+                    {
+                        "name": "Wytwarzanie oprogramowania",
+                        "gross_price": str(int(grossPrice*100)),
+                        "tax_symbol": 23
+                    }
+                ]
+            }
+        }
+        response = requests.post(self.INFAKT_API_URL + "invoices.json", headers=self._prepareHeaders(), json=payload)
+        log.info(response.status_code)
+        log.info(response.content)
+        invoice = Invoice(response.json())
+        log.info(invoice)
+        return invoice
+    
+    def deleteInvoice(self, invoiceNum):
+        response = requests.delete(self.INFAKT_API_URL + "invoices/" + str(invoiceNum) + ".json", headers=self._prepareHeaders())
+        log.info(response.status_code)
+
+        
--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 import InfaktApiClient
 import coloredlogs
 import logging
+import argparse
+
+parser = argparse.ArgumentParser(description='Infakt API Client')
+parser.add_argument('--client_id',    required=False, dest='client_id',    help='client id')
+parser.add_argument('--sale_date',    required=True,  dest='sale_date',    help='sale date')
+parser.add_argument('--invoice_date', required=False, dest='invoice_date', help='invoice date')
+parser.add_argument('--payment_date', required=True,  dest='payment_date', help='payment date')
+parser.add_argument('--gross_price',  required=True,  dest='gross_price',  help='gross price')
+parser.add_argument('--email',  required=False,  dest='email',  help='email')
+
+args = vars(parser.parse_args())
+
+client_id = args['client_id']
+if (client_id == None):
+	client_id = 9220335
+sale_date = args['sale_date']
+invoice_date = args['invoice_date']
+if (invoice_date == None):
+	invoice_date = sale_date
+payment_date = args['payment_date']
+gross_price = float(args['gross_price'])
+email = args['email']
+if (email == None):
+	email = 'marcin.marczyk@dataart.com'
 
 coloredlogs.install(level="INFO")
 log = logging.getLogger("main")
 client = InfaktApiClient.InfaktApiClient()
 
+log.info(client_id)
+log.info(sale_date)
+log.info(invoice_date)
+log.info(payment_date)
+log.info(gross_price)
+
 if __name__ == "__main__":
 	log.info('Running main application')
-	client.findInvoice(41727411)
+	invoice = client.createInvoice(client_id, sale_date, invoice_date, payment_date, gross_price)
+	client.findInvoice(invoice.id)
+	client.sendViaEmail(invoice.id, email)
+	client.generatePdf(invoice.id)
+	client.deleteInvoice(invoice.id)
+	
